157ee2c99c350bf687bdfd934400165baf59fdda
Martin Amarante <martinfamarante@gmail.com>
2018-02-08 21:13:45 -0300

    Add files via upload

---
+++ 2-InstruccionIf/2-InstruccionIf/jsInstruccionIF (1).js
@@ -0,0 +1,17 @@
+function Mostrar()
+{
+    var edad;
+    edad = document.getElementById("edad").value;
+    if(edad == 15)
+{
+    alert("niña bonita");
+}
+    if(edad == 14)
+{ 
+    alert("casi");
+}
+    if(edad == 16)
+{
+    alert("te pasaste");
+}
+}//FIN DE LA FUNCIÓN
