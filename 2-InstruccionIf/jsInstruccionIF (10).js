9dbdc288d07bae238c1552b0bfd6fca1ef6887ab
Martin Amarante <martinfamarante@gmail.com>
2018-02-15 21:33:22 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,20 @@
 function Mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
+	var nota;
+	
+		nota = Math.floor (Math.random()*10 ) + 1 ;
+		if(nota < 4 )
+		{
+			alert(nota + " Vamos, la proxima se puede");
+		}
+		else if (nota < 9 )
+		{
+			alert(nota + " Aprobaste");
+		}
+		else
+		{
+			alert(nota + " Excelente");
+		}
 	
 
-}//FIN DE LA FUNCIÓN
+}
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,10 @@
 function Mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
+	var numeroA;
+
+	numeroA = Math.floor (Math.random()*10 ) + 1 ;
+
+	alert(numeroA);
 	
 
-}//FIN DE LA FUNCIÓN
+}
