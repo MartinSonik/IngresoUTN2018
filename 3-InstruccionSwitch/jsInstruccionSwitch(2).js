2cb411d4060195881164da9c3b52dc297f1c95b8
Martin Amarante <martinfamarante@gmail.com>
2018-02-15 22:15:07 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -2,10 +2,22 @@ function Mostrar()
 {
 //tomo el mes 
 var mesDelAño = document.getElementById('mes').value;
-
-alert (mesDelAño);
-
-
-
+switch(mesDelAño)
+{   
+    case"Julio":
+    alert ("Abrigate que hace frio");
+    break;
+    case "Agosto":
+    alert ("Abrigate que hace frio");
+    break;
+    case "Junio":
+    case "Abril":
+    case "Mayo":
+    alert ("Falta para el invierno")
+    break;
+    default:
+    alert ("Ya pasamos el frio, ahora calor!!!");
+    break
+}
 
 }//FIN DE LA FUNCIÓN
