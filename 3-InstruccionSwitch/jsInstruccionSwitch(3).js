96c3d4034324f41a2fe527549c9da99c461c55c0
Martin Amarante <martinfamarante@gmail.com>
2018-02-17 13:04:19 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(3).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(3).js
@@ -1,11 +1,15 @@
 function Mostrar()
 {
-//tomo el mes 
-var mesDelAño = document.getElementById('mes').value;
+    var mesDelAño = document.getElementById('mes').value;
+    switch(mesDelAño)
+    {   
+        case"Febrero":
+        alert ("Este mes no tiene más de 29 días.");
+        break;
+        default:
+        alert ("Este mes tiene 30 o más días");
+        break
+	}
 
-//alert (mesDelAño);
-	
-	
 
-
-}//FIN DE LA FUNCIÓN
+}
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,12 +1,22 @@
 function Mostrar()
 {
-//tomo el mes 
-var mesDelAño = document.getElementById('mes').value;
-
-//alert (mesDelAño);
-	
-	
-
-
-
-}//FIN DE LA FUNCIÓN
+    var mesDelAño = document.getElementById('mes').value;
+    switch(mesDelAño)
+    {   
+        case"Febrero":
+        alert ("tiene 28 días");
+        break;
+        case "Enero":
+        case "Marzo":
+        case "Mayo":
+        case "julio":
+        case "Agosto":
+        case "Octubre":
+        case "Diciembre":
+        alert ("tiene 31 días");
+        break
+        default:
+        alert ("tiene 30 días");
+        break
+	}
+}//FIN
